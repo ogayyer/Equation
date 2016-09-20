9655525adc6bc8d7a4e0897c79f3896f29eb0ed8
Dmytro Petin <dpetin@training.local>
2016-09-20 12:06:09 +0300

    initial

---
+++ src/Equation.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Equation {
+
+	public void setup(int a, int b, int c) {
+
+	}
+
+	public int getD() {
+		int D = 0;
+
+		return D;
+	}
+
+	public boolean isRootExist() {
+
+		return false;
+	}
+
+	public Set<Integer> solve() {
+		Set<Integer> roots = new HashSet<Integer>();
+
+		return roots;
+	}
+
+	public String toString() {
+
+		return "3*xx +4*x +5 =0";
+	}
+}
---
+++ src/Maple.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Maple {
+
+	public static void main(String[] args) {
+		// setup (a, b, c)
+		// solve ()  => [x1, x2]
+		// isRootExist
+		// getD()
+
+	}
+}
+
